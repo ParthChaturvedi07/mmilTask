d2089afbcb24fc180e8ebdbd1df58752aa3ffb50
Parth Chaturvedi <155241893+ParthChaturvedi07@users.noreply.github.com>
2024-04-14 12:24:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('.main'),
+        smooth: true
+    });
+});
