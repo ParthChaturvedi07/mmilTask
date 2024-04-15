95c14c7bee74c0c1bec42f55e51c8633588d1aa6
Parth Chaturvedi <155241893+ParthChaturvedi07@users.noreply.github.com>
2024-04-15 11:53:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
         el: document.querySelector('.main'),
         smooth: true
     });
+
+const aboutLink = document.querySelector('.about');
+const contactLink = document.querySelector('.contact');
+const aboutSection = document.querySelector('.about-section');
+const contactSection = document.querySelector('.contact-section');
+
+aboutLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    scroll.scrollTo(aboutSection);
+});
+
+contactLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    scroll.scrollTo(contactSection);
+});
+
 });
